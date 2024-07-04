53e23341190f0ed7fbb691a3b44a140883b17e47
AizuAyari <s1310042@u-aizu.ac.jp>
2024-07-04 21:37:26 +0900

    HoT: initial revision

---
+++ game.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const char *toss_coin() { return rand() % 2 == 0 ? "Heads" : "Tails"; }
+
+int main() {
+  srand(time(0));
+
+  printf("Tossing a coin...\n");
+
+  int heads_count = 0, tails_count = 0;
+  const char *results[3];
+
+  for (int i = 0; i < 3; i++) {
+    results[i] = toss_coin();
+    printf("Round %d: %s\n", i + 1, results[i]);
+    if (results[i][0] == 'H')
+      heads_count++;
+    else
+      tails_count++;
+  }
+
+  printf("Heads: %d, Tails: %d\n", heads_count, tails_count);
+
+  return 0;
+}
